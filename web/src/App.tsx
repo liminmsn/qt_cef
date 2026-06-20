cb500c96c1199a5acdd177ffd9675c5ad1f23932
杨再涛 <1772081518@qq.com>
2026-06-20 23:22:08 +0800

    更新主题自定义

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,12 @@
 import { RouterProvider } from 'react-router'
+import { ThemeProvider } from "next-themes";
 import router from './router'
 import './global.css'
 
-export default function() {
-  return <div className='p-2'>
-    <RouterProvider router={router} />
-  </div>
+export default function () {
+  return <ThemeProvider attribute="class" defaultTheme='dark'>
+    <div className='p-2' data-theme="green">
+      <RouterProvider router={router} />
+    </div>
+  </ThemeProvider>
 }
