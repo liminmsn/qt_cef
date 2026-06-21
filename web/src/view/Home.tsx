3deb3c81ea573192cab8c0327c43f20e9d3b5f40
杨再涛 <1772081518@qq.com>
2026-06-21 16:13:05 +0800

    初始化shader测试项目

--- web/src/view/Home.tsx
+++ web/src/view/Home.tsx
@@ -1,5 +1,8 @@
-import { Button, Card, Label, Switch, Tabs } from '@heroui/react';
+import { Button, Card, Switch, Tabs } from '@heroui/react';
+import Editor from "@monaco-editor/react";
+import { Canvas } from "@react-three/fiber";
 import { QWebChannel } from 'qwebchannel';
+import * as THREE from 'three';
 export default function () {
 
     function test() {
@@ -11,15 +14,26 @@ export default function () {
         );
     }
 
+    const code = `
+uniform float time;
+
+void main()
+{
+    gl_FragColor =
+        vec4(
+            abs(sin(time)),
+            0.0,
+            1.0,
+            1.0
+        );
+}
+    `;
+
     return <Tabs className="w-full h-full pt-2">
         <Tabs.ListContainer className='w-1/2 mx-auto text-center'>
             <Tabs.List aria-label="Options" className='w-fit *:h-6 *:w-fit *:px-3 *:text-sm *:font-normal'>
                 <Tabs.Tab id="overview">
-                    视图
-                    <Tabs.Indicator />
-                </Tabs.Tab>
-                <Tabs.Tab id="analytics">
-                    分类
+                    代码
                     <Tabs.Indicator />
                 </Tabs.Tab>
                 <Tabs.Tab id="reports">
@@ -28,25 +42,46 @@ export default function () {
                 </Tabs.Tab>
             </Tabs.List>
         </Tabs.ListContainer>
-        <Tabs.Panel id="overview">
-            <Card>
-                <Switch>
-                    <Switch.Content>
-                        <Switch.Control>
-                            <Switch.Thumb />
-                        </Switch.Control>
-                        Enable notifications
-                    </Switch.Content>
-                </Switch>
-                <Label>hello world</Label>
-                <Button size="sm" onClick={test}>测试</Button>
-            </Card>
-        </Tabs.Panel>
-        <Tabs.Panel className="pt-4" id="analytics">
-            <p>Track your metrics and analyze performance data.</p>
+        <Tabs.Panel id="overview" className='h-full'>
+            <div className='grid grid-cols-2 space-x-1 h-full'>
+                <div className='flex flex-col space-y-1'>
+                    <Card className='p-0! pt-4! h-full'>
+                        <Editor
+                            theme=''
+                            language="cpp"
+                            value={code}
+                        />
+                    </Card>
+                    <Card>
+                        <Switch>
+                            <Switch.Content>
+                                <Switch.Control>
+                                    <Switch.Thumb />
+                                </Switch.Control>
+                                Enable notifications
+                            </Switch.Content>
+                        </Switch>
+                        <Button size="sm" onClick={test}>执行</Button>
+                    </Card>
+                </div>
+                <Card className='p-0! overflow-clip'>
+                    <Canvas camera={{ position: [0, 0, 3] }}>
+                        <ambientLight />
+
+                        <mesh rotation={[0.5, 0.5, 0]}>
+                            <boxGeometry args={[1, 1, 1]} />
+                            <meshNormalMaterial />
+                        </mesh>
+                    </Canvas>
+                </Card>
+            </div>
         </Tabs.Panel>
         <Tabs.Panel className="pt-4" id="reports">
-            <p>Generate and download detailed reports.</p>
+            resr
         </Tabs.Panel>
     </Tabs>
+}
+
+function useMemo(arg0: () => any, arg1: never[]) {
+    throw new Error('Function not implemented.');
 }
